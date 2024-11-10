395f34755d700e0cbda21aa66161d4c34a11b415
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-11-10 14:32:32 +0000

    Completed mongoDB connection

--- myApp.js
+++ myApp.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+//connecting to mongo cluster using URI (Uniform Resource Identifier)
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+
+
 
 let Person;
 
