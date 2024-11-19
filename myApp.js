d4ab682dd5af9c0940c8f0d8fb2bacaa91399d34
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-11-19 13:39:33 +0000

    Completed update tutorial

--- myApp.js
+++ myApp.js
@@ -65,13 +65,41 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId,(err,data)=>{
+    if(err) return console.err(err);
+    done(null , data);
+  })
 };
 
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+// const findEditThenSave = (personId, done) => {
+//   const foodToAdd = "hamburger";
+//   Person.findById(personId,(err,person)=>{
+//     if(err) return console.err(err);
+//     person.favoriteFoods.push(foodToAdd);
+//     person.save((err,updatedPerson)=>{
+//       if(err) return console.error(err);
+//       console.log(updatedPerson)
+//       done(null , updatedPerson);
+//     })  
+//   });
+// };
 
-  done(null /*, data*/);
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = 'hamburger';
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
